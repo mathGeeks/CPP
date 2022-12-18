a04f42e8272d026977fe28274329a7c861d02753
40843245 <75050655+40843245@users.noreply.github.com>
2022-12-18 18:42:37 +0800

    Add files via upload

---
+++ Library/valarray/slice_ex1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cstddef>
+#include <valarray>
+using namespace std;
+
+template <class T>
+void PrintValarray(valarray<T> va, string name)
+{
+    cout<<name<<":"<<endl;
+    for(int i=0;i<va.size();i++)
+    {
+        cout<<va[i]<<" ";
+    }
+    cout<<endl;
+}
+int main ()
+{
+  std::valarray<int> foo (12);
+  for (int i=0; i<12; ++i)
+  {
+      foo[i]=i*i;
+  }
+
+  std::slice second (2,3,4);
+  std::slice third (second);
+
+  std::valarray<int> bar = foo[third];
+
+  PrintValarray<int>(foo,"foo");
+  cout<<"---------"<<endl;
+  PrintValarray<int>(bar,"bar");
+  cout<<"---------"<<endl;
+  return 0;
+}
