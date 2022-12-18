abad9acd76f99d4822b50bf8f56bc266e03c4b36
40843245 <75050655+40843245@users.noreply.github.com>
2022-12-18 09:04:03 +0800

    Add files via upload

---
+++ Library/Algorithm/next_permutation_ex1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>    
+#include <algorithm>    
+using namespace std;
+
+int main () 
+{
+  int myints[] = {1,2,3};
+  int n=sizeof(myints)/sizeof(myints[0]);
+  int idx=1;
+  sort(myints,myints+n);
+  cout << "The possible permutations:"<<endl;
+  do 
+  {
+  	cout<<"---------"<<endl;
+  	cout<<"the "<<idx<<" th solution:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+    	cout<<myints[i];
+    }
+    cout<<endl;
+    cout<<"---------"<<endl;
+    idx++;
+  } while ( std::next_permutation(myints,myints+n) );
+
+  cout << "After loop: "<<endl';
+  cout<<"The array is:"<<endl;
+  cout<<"the "<<idx<<" th solution:"<<endl;
+  for(int i=0;i<n;i++)
+  {
+    	cout<<myints[i];
+  }
+  cout<<endl;
+
+  return 0;
+}
